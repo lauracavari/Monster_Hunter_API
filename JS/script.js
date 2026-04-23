315f006f8fc21ecf7d022a752d46b771fb68c4fd
MonoInGithub <160603337+MonoInGithub@users.noreply.github.com>
2026-04-23 17:50:00 +0200

    Limite de 20 añadidos

--- JS/script.js
+++ JS/script.js
@@ -19,16 +19,16 @@ $(document).ready(function(){
             
             let usersArr = data;
 
-            usersArr.forEach(function(userData){
+            for(let i=0; i<20; i++){
                 let card = `
                 <div class="col-xxl-3 col-md-4 col-sm-12">
                     <div class="card h-100 user-card shadow align-items center p-2">
                         <img src="Media/PlaceHolders.webp"
                                 class="card-img-top user-img  p-3"
-                                alt="${userData.id}">
+                                alt="${usersArr[i].id}">
                         <div class="card-body">
-                            <h5 class="card-title">${userData.name}</h5>
-                            <p class="card-text">${userData.species}</p>
+                            <h5 class="card-title">${usersArr[i].name}</h5>
+                            <p class="card-text">${usersArr[i].species}</p>
                             <a href="#" class="btn btn-primary">Go somewhere</a>
                         </div>
                     </div>
@@ -36,7 +36,7 @@ $(document).ready(function(){
             `;
 
             $cont.append(card);
-            });
+            }
         })
 
 })
