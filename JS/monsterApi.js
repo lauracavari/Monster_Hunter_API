b89cbf9546337a1fd15711234fd9d83b338c3bc2
Cinsocraft <cinoscraft@gmail.com>
2026-04-28 13:49:57 +0200

    push save JQUERRY
    Trabajando en el jquerry y java, para evitar perdermi progreso.

---
+++ JS/monsterApi.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    console.log("Online y con jQuery");
+
+    const URLParams = new URLSearchParams(window.location.search);
+    const monsterID = URLParams.get("id");
+
+    const gameCont = $("#monsterDetailCont");
+
+    fetch("https://mhw-db.com/monsters" + gameID)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(result){
+            console.log(result);
+
+            let gameData = result.data;
+            let gameDetailsHTML = `
+                <div class="col-md-12 col-lg-4 text-center">
+                    <img src="media/img/${gameID}.jpg" alt="${gameData.name}" class="img-fluid">
+                </div>
+                <div class="col-md-12 col-lg-8">
+                    <h1>${gameData.name}</h1>
+                    <p>${gameData.description}</p>
+                </div>
+            `
+
+            gameCont.append(gameDetailsHTML);
+        })
+        .catch(function(){
+
+        })
+        .finally(function(){
+
+        })
+});
